9aa5207b8090b98733d693818fb83f83e44ef047
Jan Wroblewski <xi@mimuw.edu.pl>
2019-07-31 23:09:52 +0200

    Updated benchmark output.

--- benchmark.sh
+++ benchmark.sh
@@ -35,11 +35,23 @@ function inf {
 function bench {
   while read IN; do
     OUT=($(inf $IN 2>&1 > /dev/null | grep "real\|code" | tail -n 2 | sort | awk '{print $2}'))
-    printf "%-40s %-15s %s\n" "${IN#benchmark/}" "${OUT[1]}" "${OUT[0]}"
+    TIME="${OUT[1]}"
+    if [[ ${OUT[0]} == 0 ]]; then
+      RESULT="INF"
+    elif [[ ${OUT[0]} == 1 ]]; then
+      RESULT="FIN"
+    elif [[ ${OUT[0]} == 2 ]]; then
+      RESULT="FIN (UNSAFE)"
+    elif [[ ${OUT[0]} == 143 ]]; then
+      RESULT="SIGTERM"
+    else
+      RESULT="ERROR"
+    fi
+    printf "%-40s %-15s %s\n" "${IN#benchmark/}" "${TIME}" "$RESULT"
   done
 }
 
 echo "Benchmark of infsat $OPTS benchmark/* with timeout ${TIMEOUT}s"
 echo "$(date +%Y-%m-%d) $(lscpu | sed -nr '/Model name/ s/.*:\s*(.*) @ .*/\1/p')"
-echo "--- input file ------------------------- time ---------- output code ---"
+echo "--- input file ------------------------- time ---------- result ---"
 find "$DIR/" -name "*.inf" -type f | bench
